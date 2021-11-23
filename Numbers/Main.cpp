6040d588193fa3ffbd7e55ca153e7cc9a994acba
Crosslywere <ogboruj@outlook.com>
2021-11-23 13:09:48 +0100

    Numbers working

--- Numbers/Main.cpp
+++ Numbers/Main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
-#define NUMBERS 10
+#define NUMBERS 11
 
 class Number
 {
@@ -25,14 +25,65 @@ Number data[NUMBERS] = {
     {0, "zero"}, {1, "one"}, {2, "two"},
     {3, "three"}, {4, "four"}, {5, "five"},
     {6, "six"}, {7, "seven"}, {8, "eight"},
-    {9, "nine"}
+    {9, "nine"}, {10, "ten"}
 };
+
+bool isNum(std::string str)
+{
+    for(char n : str)
+    {
+        if((n < '0' || n > '9') && n != ',')
+            return false;
+    }
+    return true;
+}
+
+uint64_t toNum(std::string str)
+{
+    uint64_t result = 0;
+    for(char n : str)
+    {
+        if(n == ',')
+            continue;
+        result *= 10;
+        result += n - '0';
+    }
+    return result;
+}
+
+uint64_t getValue(std::string word)
+{
+    for(int i = 0; i < NUMBERS; i++)
+    {
+        if(data[i].getWord() == word)
+            return data[i].getValue();
+    }
+    return Number(0).getValue();
+}
+
+std::string getWord(std::string value)
+{
+    uint64_t val = toNum(value);
+    for(int i = 0; i < NUMBERS; i++)
+    {
+        if(data[i].getValue() == val)
+            return data[i].getWord();
+    }
+    return Number(0).getWord();
+}
 
 int main()
 {
-    for (int i = 0; i < NUMBERS; i++)
+    std::string input;
+    std::cout << "Enter a number as word or as a value: ";
+    std::getline(std::cin, input);
+    if (!isNum(input))
     {
-        std::cout << data[i] << std::endl;
+        std::cout << input << " is " << getValue(input.) << std::endl;
+    }
+    else
+    {
+        std::cout << input << " is " << getWord(input) << std::endl;
     }
     system("pause");
     return 0;
