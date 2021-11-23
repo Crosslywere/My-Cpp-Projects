4b7f29d30e7763a53338af28c9b9858b1dbdf4a7
Crosslywere <ogboruj@outlook.com>
2021-11-23 13:43:05 +0100

    Fixed code and added more numbers

--- Numbers/Main.cpp
+++ Numbers/Main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
-#define NUMBERS 11
+#define NUMBERS 30
 
 class Number
 {
@@ -25,7 +25,13 @@ Number data[NUMBERS] = {
     {0, "zero"}, {1, "one"}, {2, "two"},
     {3, "three"}, {4, "four"}, {5, "five"},
     {6, "six"}, {7, "seven"}, {8, "eight"},
-    {9, "nine"}, {10, "ten"}
+    {9, "nine"}, {10, "ten"}, {11, "eleven"},
+    {12, "twelve"}, {13, "thirteen"}, {14, "fourteen"},
+    {15, "fifteen"}, {16, "sixteen"}, {17, "seventeen"},
+    {18, "eighteen"}, {19, "nineteen"}, {20, "twenty"},
+    {30, "thirty"}, {40, "fourty"}, {50, "fifty"},
+    {60, "sixty"}, {70, "seventy"}, {80, "eighty"},
+    {90, "ninety"}, {100, "hundred"}
 };
 
 bool isNum(std::string str)
@@ -79,7 +85,7 @@ int main()
     std::getline(std::cin, input);
     if (!isNum(input))
     {
-        std::cout << input << " is " << getValue(input.) << std::endl;
+        std::cout << input << " is " << getValue(input) << std::endl;
     }
     else
     {
