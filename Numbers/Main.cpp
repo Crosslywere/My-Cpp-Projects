d6af9f28ff118cec7c2321ca13fdf40c632b03fc
Crosslywere <ogboruj@outlook.com>
2021-12-01 16:53:41 +0100

    Completed number value to word conversion

--- Numbers/Main.cpp
+++ Numbers/Main.cpp
@@ -1,95 +1,21 @@
 #include <iostream>
 #include <cstdlib>
-#include <string>
-#define NUMBERS 30
-
-class Number
-{
-private:
-    uint64_t value;
-    std::string word;
-public:
-    Number(const uint64_t& value, const std::string& word = "Undefined")
-        : value(value), word(word) {}
-    uint64_t getValue() const { return value; }
-    std::string getWord() const { return word; }
-};
-
-std::ostream& operator<<(std::ostream& out, const Number& number)
-{
-    out << number.getValue() << ", " << number.getWord();
-    return out;
-}
-
-Number data[NUMBERS] = {
-    {0, "zero"}, {1, "one"}, {2, "two"},
-    {3, "three"}, {4, "four"}, {5, "five"},
-    {6, "six"}, {7, "seven"}, {8, "eight"},
-    {9, "nine"}, {10, "ten"}, {11, "eleven"},
-    {12, "twelve"}, {13, "thirteen"}, {14, "fourteen"},
-    {15, "fifteen"}, {16, "sixteen"}, {17, "seventeen"},
-    {18, "eighteen"}, {19, "nineteen"}, {20, "twenty"},
-    {30, "thirty"}, {40, "fourty"}, {50, "fifty"},
-    {60, "sixty"}, {70, "seventy"}, {80, "eighty"},
-    {90, "ninety"}, {100, "hundred"}, {1000, "thousand"};
-};
-
-bool isNum(std::string str)
-{
-    for(char n : str)
-    {
-        if((n < '0' || n > '9') && n != ',')
-            return false;
-    }
-    return true;
-}
-
-uint64_t toNum(std::string str)
-{
-    uint64_t result = 0;
-    for(char n : str)
-    {
-        if(n == ',')
-            continue;
-        result *= 10;
-        result += n - '0';
-    }
-    return result;
-}
-
-uint64_t getValue(std::string word)
-{
-    for(int i = 0; i < NUMBERS; i++)
-    {
-        if(data[i].getWord() == word)
-            return data[i].getValue();
-    }
-    return Number(0).getValue();
-}
-
-std::string getWord(std::string value)
-{
-    uint64_t val = toNum(value);
-    for(int i = 0; i < NUMBERS; i++)
-    {
-        if(data[i].getValue() == val)
-            return data[i].getWord();
-    }
-    return Number(0).getWord();
-}
+#include "NumbersUtil.h"
 
 int main()
 {
-    std::string input;
-    std::cout << "Enter a number as word or as a value: ";
-    std::getline(std::cin, input);
-    if (!isNum(input))
+    NumberArray numData;
+    initData(numData);
+    Number input;
+    std::cout << "Enter a number between 0 and 99: ";
+    std::cin >> input.word;
+    if(convToVal(input.word, input.value))
     {
-        std::cout << input << " is " << getValue(input) << std::endl;
+        std::cout << input.value << " is " << genWord(input.value) << std::endl;
     }
     else
     {
-        std::cout << input << " is " << getWord(input) << std::endl;
+        std::cout << input.word << " is " << genValue(input.word) << std::endl;
     }
     system("pause");
     return 0;
---
+++ Numbers/NumbersUtil.h
@@ -0,0 +1,214 @@
+/**
+ * This file contains the functions needed to get numbers as words or as integers
+ * using loosely defined values.
+ * These values are all values that are uniquely spelled in a single word. These are
+ * 0 to 20 then it goes in tens to 100 and finally defines 1000.
+ * These are all defined in the NumberData.txt text file.
+ * Any extra data that you want to define can be added by using the loadExtraData fuction
+ * after using the initData function.
+ * 
+ */
+#pragma once
+#ifndef NUMBERS_UTIL_H
+#define NUMBERS_UTIL_H
+
+/* Includes */
+
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+
+
+/* Defines and Constants */
+
+#define DATAFILE "NumberData.txt"
+#define STREAM_SIZE 30
+#define INIT_DATA_SIZE 30
+
+
+/* Declearations */
+
+// Struct for Number data
+struct Number;
+// For loading the inital definitions of numbers form a predetermined file
+// (called before any other function)
+void initData(std::vector<Number>&);  
+// Loads extra Number data into a vector from a text file
+void loadExtraData(std::string&&, std::vector<Number>&);
+// Extracting struct Number from a single line string
+Number extractData(std::string);
+// Returns true if an unsigned long long can be extracted
+bool convToVal(std::string, uint64_t&);
+// Function for getting the value representation of a string of words from a data set
+uint64_t genValue(std::string, const std::vector<Number>&);
+// Function for getting the word represtentation of an integer from a data set
+std::string genWord(uint64_t, const std::vector<Number>&);
+
+std::vector<Number>* numberData;
+
+
+/* Typedefs and Usings */
+
+typedef std::vector<Number> NumberArray;
+using Word = std::string;
+using Value = unsigned long long;
+
+
+/* Definitions */
+
+struct Number
+{
+    unsigned long long value;
+    std::string word;
+};
+
+void initData(NumberArray& data)
+{
+    char stream[STREAM_SIZE];
+    std::ifstream dataFile;
+    dataFile.open(DATAFILE);
+    while(dataFile.good())
+    {
+        dataFile.getline(stream, STREAM_SIZE, '\n');
+        if(std::string(stream).empty())
+            continue;
+        data.emplace_back(extractData(std::string(stream)));
+    }
+    dataFile.close();
+    numberData = &data;
+}
+
+Number extractData(std::string stream)
+{
+    Number result;
+    std::stringstream ss(stream);
+    while(ss.good())
+    {
+        std::string str;
+        ss >> str;
+        if (!convToVal(str, result.value))
+            result.word = str;
+    }
+    return result;
+}
+
+bool convToVal(const Word str, Value& value)
+{
+    uint64_t temp = 0;
+    for(char c : str)
+    {
+        if(c < '0' || c > '9')
+        {
+            return false;
+        }
+        else
+        {
+            temp *= 10;
+            temp += c - '0';
+        }
+    }
+    value = temp;
+    return true;
+}
+
+void loadExtraData(std::string&& file, NumberArray& data = *numberData)
+{
+    if (data.size() == INIT_DATA_SIZE) // TODO: Poor safeguard(Should probably have a static variable to check)
+    {
+        char stream[STREAM_SIZE];
+        std::ifstream extraData;
+        extraData.open(file);
+        while(extraData.good())
+        {
+            extraData.getline(stream, STREAM_SIZE, '\n');
+            if(std::string(stream).empty() || stream[0] == '#')
+                continue;
+            data.emplace_back(extractData(std::string(stream)));
+        }
+        extraData.close();
+    }
+}
+
+// Other useful functions
+
+// Extract right most segment
+unsigned short extractRMS(Value& value)
+{
+    unsigned short right = value % 1000;
+    value /= 1000;
+    return right;
+}
+
+// Divides the value into segments and returns it as a vector of values
+std::vector<Value> segment(Value value)
+{
+    std::vector<Value> result;
+    while(value > 0)
+    {
+        result.emplace_back(0);
+        result.back() += extractRMS(value);
+    }
+    return result;
+}
+
+Word genWord(Value value, NumberArray data = *numberData)
+{
+    // If the number is greater than or equal to 1000 it has segments so it should be segmented
+    if(value >= 1000)
+    {
+        std::vector<Value> segments = segment(value);
+        Word words = "";
+        for(int i = segments.size() - 1; i >= 0; i--)
+        {
+            // Recursively concatonate each segment
+            words += genWord(segments[i]) + (i == 0 ? "" : " " + data[data.size() - i].word + " ");
+        }
+        return words;
+    }
+    // When the smallest segment is passed and can be identified immediately ie numbers between 0 and 90
+    for(int i = 0; i < INIT_DATA_SIZE - 2; i++)
+    {
+        if(value == data[i].value)
+            return data[i].word;
+    }
+    // Gets to this point if the number is not immediately identified
+    // Getting the hundred value and storing it in temp
+    short temp = value / 100;
+    value %= 100;
+    // If the temp holds a hundred value
+    if(temp > 0)
+    {
+        // Returns the hundred value and concatonates the next value
+        return genWord(temp) + " hundred " + genWord(value);
+    }
+    else // If the value doesn't have a hundred value
+    {
+        // Gets the unit
+        temp = value % 10;
+        return genWord(value - temp) + (temp == 0 ? "" : " " + genWord(temp));
+    }
+    // If all else fails returns Unknown
+    return "Unknown";
+}
+
+Value genValue(Word word, NumberArray data = *numberData)
+{
+    return 0;
+}
+
+
+/* Other */
+
+#if defined(_GLIBCXX_IOSTREAM) || defined(_IOSTREAM_) // _GLIBCXX_IOSTREAM for MingW and _IOSTREAM_ for MSVC
+// Printing the struct Number using the default basic_ostream
+std::ostream& operator<<(std::ostream& out, Number number)
+{
+    out << number.value << " " << number.word;
+    return out;
+}
+#endif
+
+#endif // NUMBERS_UTIL_H
